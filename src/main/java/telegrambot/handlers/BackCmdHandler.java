bf1c31022efdcdf8d159c78732cacc91e4fcce67
BerserkrMM <mr.monopoly.od@gmail.com>
2023-04-29 13:30:45 +0300

    wip

--- src/main/java/telegrambot/handlers/BackCmdHandler.java
+++ src/main/java/telegrambot/handlers/BackCmdHandler.java
@@ -5,14 +5,27 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import telegrambot.config.interceptor.AdditionalUserPropertiesContextHolder;
+import telegrambot.repository.util.CommandStateDependencyRepository;
+import telegrambot.repository.util.CurrentConditionRepository;
 
 @AllArgsConstructor
 @Component
-public class BackCmdHandler  extends AbstractCmdHandler{
+public class BackCmdHandler extends AbstractCmdHandler {
     private static final String THIS_CMD = "/back";
+    private final CurrentConditionRepository currentConditionRepository;
+    private final CommandStateDependencyRepository commandStateDependencyRepository;
 
     @Override
     public SendMessage processMessage() {
+        Update update = AdditionalUserPropertiesContextHolder.getContext().getUpdate();
+        var currentCondition = currentConditionRepository.getFirst();
+        var currentCommand = currentCondition.getCommand();
+        var currentState = currentCondition.getState();
+        var commandStateDependency = commandStateDependencyRepository
+                .findByCurCommandAndCurSate(currentCommand, currentState);
+        var previousState = commandStateDependency.getPreviousState();
+
+
         return null;
     }
 
