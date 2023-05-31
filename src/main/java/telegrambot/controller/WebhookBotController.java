b7420fc7a57d19aaed83cd78ee0eb85e9f9a0a54
Valerii Kovalenko <valera23ps4@gmail.com>
2023-05-31 12:32:04 +0300

    feat: added the query time for WebhookBotController

--- src/main/java/telegrambot/controller/WebhookBotController.java
+++ src/main/java/telegrambot/controller/WebhookBotController.java
@@ -2,21 +2,42 @@ package telegrambot.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import telegrambot.config.telegram.TelegramWalletBot;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/webhook")
 public class WebhookBotController {
 
     private final TelegramWalletBot telegramWalletBot;
 
-    @PostMapping
-    public SendMessage receiveUpdate(@RequestBody Update update) {
-        return telegramWalletBot.onWebhookUpdateReceived(update);
+    @PostMapping(value = "/go")
+    public ResponseEntity<SendMessage> receiveUpdate(@RequestBody Update update) {
+
+        Duration duration = getDurationBetweenRequestAndCurrentTime(update);
+
+        if (duration.toHours() >= 12) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.ok(telegramWalletBot.onWebhookUpdateReceived(update));
+    }
+
+    private Duration getDurationBetweenRequestAndCurrentTime(Update update) {
+
+        long unixTime = update.getMessage().getDate().longValue();
+        Instant requestTime = Instant.ofEpochSecond(unixTime);
+        Instant currentTime = Instant.now();
+
+        return Duration.between(requestTime, currentTime);
     }
 }
