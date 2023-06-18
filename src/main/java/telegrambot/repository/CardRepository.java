c9aac89373079ce4d1bb69d1bce4813a415fa69b
Valerii Kovalenko <valera23ps4@gmail.com>
2023-06-18 19:30:25 +0300

    wip: Override method getById

--- src/main/java/telegrambot/repository/CardRepository.java
+++ src/main/java/telegrambot/repository/CardRepository.java
@@ -17,6 +17,10 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     @Query(value = "SELECT c.id, c.name, c.balance  FROM cards c WHERE c.name = :name", nativeQuery = true)
     Optional<Card> getByName(@Param("name") String name);
 
+    @Override
+    @Query(value = "SELECT c.id, c.name, c.balance  FROM cards c WHERE c.id = :id", nativeQuery = true)
+    Card getById(@Param("id") Long id);
+
     @Transactional
     @Modifying
     @Query(value = "UPDATE cards SET balance = :amount WHERE name = :name", nativeQuery = true)
